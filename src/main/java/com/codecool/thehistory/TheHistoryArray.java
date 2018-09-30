4585ff080ad3be6bda4843137de1e02d0186a9cb
David Szentesi <szntsd@gmail.com>
2018-09-30 17:33:42 +0200

    array implementations

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -12,27 +12,51 @@ public class TheHistoryArray implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        wordsArray = text.split("\\s+");
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        int countOccurrence = 0;
+        for (String word : wordsArray) {
+            if (word.equals(wordToBeRemoved)) {
+                countOccurrence++;
+            }
+        }
+
+        String[] newWordsArray = new String[wordsArray.length - countOccurrence];
+        int newWordsArrayIndex = 0;
+        for (String word : wordsArray) {
+            if (!word.equals(wordToBeRemoved)) {
+                newWordsArray[newWordsArrayIndex] = word;
+                newWordsArrayIndex++;
+            }
+        }
+
+        wordsArray = newWordsArray;
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArray.length;
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsArray = new String[0];
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+        for (int i = 0; i < wordsArray.length; i++) {
+            if (wordsArray[i].equals(from)) {
+                wordsArray[i] = to;
+            }
+        }
     }
 
     @Override
