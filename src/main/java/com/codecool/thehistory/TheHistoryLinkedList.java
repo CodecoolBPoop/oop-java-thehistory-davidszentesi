04b259c4839de3e8a7479fc50703bd3f522c52c3
David Szentesi <szntsd@gmail.com>
2018-09-30 19:44:30 +0200

    linkedlist implementations

--- src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
@@ -1,5 +1,6 @@
 package com.codecool.thehistory;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,27 +15,31 @@ public class TheHistoryLinkedList implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        wordsLinkedList.addAll(Arrays.asList(text.split("\\s+")));
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        wordsLinkedList.removeIf(wordToBeRemoved::equals);
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsLinkedList.size();
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsLinkedList.clear();
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+        Collections.replaceAll(wordsLinkedList, from, to);
     }
 
     @Override
